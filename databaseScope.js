feee04a6ea2796543fe3704bac95aaaf8cc75181
Devesh Kumar <contact@devesh.tech>
2020-11-29 12:33:10 +0530

    Added database scope wrapper for server environments.

--- databaseScope.js
+++ databaseScope.js
@@ -6,10 +6,22 @@ function databaseScope() {
 
 	this.getItem = function(itemToGet = "") {
 		if (!isServer) return abstraction.getItem(itemToGet);
+		else {
+			let fileName = `${itemToGet}.json`;
+			let fileContents = fs.readFileSync(fileName, { encoding: "utf8" });
+			return JSON.parse(fileContents);
+		}
 	};
 
-	this.setItem = function(itemToSet = "") {
+	this.setItem = function(itemToSet = "", contentToSet = "") {
 		if (!isServer) return abstraction.setItem(itemToSet);
+		else {
+			let fileName = `${itemToGet}.json`;
+			let fileContents = fs.writeFileSync(fileName, contentToSet, {
+				encoding: "utf8",
+			});
+			return true;
+		}
 	};
 }
 
