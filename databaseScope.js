735aa476e64effdfacde2359eafd6adcd534473a
Devesh Kumar <contact@devesh.tech>
2020-11-29 12:48:32 +0530

    Added docs for the new database scope file.

--- databaseScope.js
+++ databaseScope.js
@@ -1,27 +1,53 @@
 const isServer = require("./isServer");
 
+/**
+	Function that returns a set of functions to interact with the low level storage of the database.
+
+	On a browser that will be the localStorage, and on a server it will be the actual native file system.
+
+	@return { Object } { getItem: function, setItem: function }
+*/
 function databaseScope() {
 	let isServer = isServer();
-	let abstraction = isServer ? window.localStorage : {};
+	let abstraction = isServer ? window.localStorage : fs;
 
 	return {
+		/**
+			Function to get contents from the localStorage on the browser and `itemToGet.json` file from the server.
+
+			@param { String } itemToGet - The name of the file or item from localStorage to get.
+
+			@return { String } The contents of the file or the item.
+		*/
 		getItem: function(itemToGet = "") {
 			if (!isServer) return abstraction.getItem(itemToGet);
 			else {
 				let fileName = `${itemToGet}.json`;
-				let fileContents = fs.readFileSync(fileName, {
+				let fileContents = abstraction.readFileSync(fileName, {
 					encoding: "utf8",
 				});
-				return JSON.parse(fileContents);
+				return fileContents;
 			}
 		},
+		/**
+			Function to get contents from the localStorage on the browser and `itemToGet.json` file from the server.
+
+			@param { String } itemToSet - The name of the file or item from localStorage to get.
+			@param { String } contentToSet - The serialized contents to set in the localStorage and file system.
+
+			@return { Boolean } Results of the operation.
+		*/
 		setItem: function(itemToSet = "", contentToSet = "") {
 			if (!isServer) return abstraction.setItem(itemToSet);
 			else {
 				let fileName = `${itemToGet}.json`;
-				let fileContents = fs.writeFileSync(fileName, contentToSet, {
-					encoding: "utf8",
-				});
+				let fileContents = abstraction.writeFileSync(
+					fileName,
+					contentToSet,
+					{
+						encoding: "utf8",
+					}
+				);
 				return true;
 			}
 		},
